7614af3bc3aa0f1cdb33f38016a19592cab86974
Salvatore Esposito <salvatore.esposito78@gmail.com>
2020-01-30 01:16:28 +0100

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+const Handlebars = require('handlebars');
+
+var source = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+
+var context = { title: "My New Post", body: "This is my first post!" };
+var html = template(context);
+var content = document.getElementById('content');
+content.innerHTML = html;
